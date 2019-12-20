74994053429219a25dfe7d50affc9f24852bc9f4
Evan Jameson <ecjameson@gmail.com>
2019-12-20 12:11:10 -0800

    Finished create table statements and added drop table statements for testing

---
+++ Database/drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE Meets_Schools;
+DROP TABLE Results_Meets;
+DROP TABLE Results_Events;
+DROP TABLE Athletes_Results;
+DROP TABLE Athletes_Schools;
+
+DROP TABLE Events;
+DROP TABLE Meets;
+DROP TABLE Schools;
+DROP TABLE Results;
+DROP TABLE Athletes;
--- Database/schema.sql
+++ Database/schema.sql
@@ -1,8 +1,8 @@
----------------
---ENTITY SETS--
----------------
+###############
+##ENTITY SETS##
+###############
 
---Stores information about individual Athletes
+##Stores information about individual Athletes
 CREATE TABLE Athletes(
   AthleteID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Name VARCHAR(100) NOT NULL,
@@ -10,12 +10,12 @@ CREATE TABLE Athletes(
   Grade ENUM('9th Grade','10th Grade','11th Grade','12th Grade','freshman', 'sophomore','junior','senior') NOT NULL
 ) AUTO_INCREMENT = 1;
 
---Stores information about individual Results
+##Stores information about individual Results
 CREATE TABLE Results(
   ResultID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Position INT NOT NULL,
-  TimeMark TIME,          --for running events
-  DistanceMarkInches INT, --for field events (storing as inches to easily convert back while also maintaining comparator)
+  TimeMark TIME,          ##for running events
+  DistanceMarkInches INT, ##for field events (storing as inches to easily convert back while also maintaining comparator)
   Pr boolean NOT NULL,
   Sr boolean NOT NULL,
   Wind decimal NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE Results(
   Season VARCHAR(100) NOT NULL
 ) AUTO_INCREMENT = 1;
 
---Stores information about individual Schools
+##Stores information about individual Schools
 CREATE TABLE Schools(
   SchoolID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Name VARCHAR(100) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE Schools(
   Location VARCHAR(100) NOT NULL
 ) AUTO_INCREMENT = 1;
 
---Stores information about individual Meets
+##Stores information about individual Meets
 CREATE TABLE Meets(
   MeetID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   Name VARCHAR(100) NOT NULL,
@@ -39,28 +39,54 @@ CREATE TABLE Meets(
   Sport ENUM('xc','tf') NOT NULL
 ) AUTO_INCREMENT = 1;
 
+##Stores information about individual Events
+CREATE TABLE Events(
+  EventID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Name VARCHAR(100)
+) AUTO_INCREMENT = 1;
 
----------------------
---RELATIONSHIP SETS--
----------------------
+#####################
+##RELATIONSHIP SETS##
+#####################
 
---Maintains the Many-to-Many relationship between Athletes and Schools
+##Maintains the Many-to-Many relationship between Athletes and Schools
 CREATE TABLE Athletes_Schools(
-  AthleteID int NOT NULL,
-  SchoolID int NOT NULL,
+  AthleteID INT NOT NULL,
+  SchoolID INT NOT NULL,
   PRIMARY KEY(AthleteId, SchoolId),
-  FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID)
+  FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
   FOREIGN KEY (SchoolID) REFERENCES Schools(SchoolID)
 );
 
---Maintains the Many-to-Many relationship between Athletes and Schools
+##Maintains the Many-to-Many relationship between Athletes and Results
 CREATE TABLE Athletes_Results(
-  AthleteID int NOT NULL,
-  ResultID int NOT NULL,
+  AthleteID INT NOT NULL,
+  ResultID INT NOT NULL,
   PRIMARY KEY(AthleteId, ResultId),
-  FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID)
+  FOREIGN KEY (AthleteID) REFERENCES Athletes(AthleteID),
   FOREIGN KEY (ResultID) REFERENCES Results(ResultID)
 );
 
+##Maintains the Many-to-One relationship between Results and Events
+CREATE TABLE Results_Events(
+  ResultID INT NOT NULL PRIMARY KEY,
+  EventID INT NOT NULL,
+  FOREIGN KEY (ResultID) REFERENCES Results(ResultID),
+  FOREIGN KEY (EventID) REFERENCES Events(EventID)
+);
 
+##Maintains the Many-to-One relationship between Results and Meets
+CREATE TABLE Results_Meets(
+  ResultID INT NOT NULL PRIMARY KEY,
+  MeetID INT NOT NULL,
+  FOREIGN KEY (ResultID) REFERENCES Results(ResultID),
+  FOREIGN KEY (MeetID) REFERENCES Meets(MeetID)
+);
 
+##Maintains the Many-to-One relationship between Meets and Schools
+CREATE TABLE Meets_Schools(
+  MeetID INT NOT NULL PRIMARY KEY,
+  SchoolID INT NOT NULL,
+  FOREIGN KEY (MeetID) REFERENCES Meets(MeetID),
+  FOREIGN KEY (SchoolID) REFERENCES Schools(SchoolID)
+);
